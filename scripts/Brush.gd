4800e867894b70b4373a071250fb40a4dca416af
Nikita Nigmatullin <nnigmat@gmail.com>
2020-10-20 11:28:00 +0300

    Add counting of paints on the canvas

--- scripts/Brush.gd
+++ scripts/Brush.gd
@@ -1,13 +1,13 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal painted()
+
 export(String, "Solid", "Dashed") var stroke_type
 export(float, 0, 10, 0.01) var stroke_width: float = 1 
 export(Color, RGB) var stroke_color = Color(0, 0, 0)
 export(Mesh) var stroke_shape = PlaneMesh.new()
 export(float, 0, 1, 0.01) var stroke_flow: float = 1
+export(bool) var signal_painted = true 
 	
 var timer = 0
 export var is_draw: bool = true
@@ -31,6 +31,9 @@ func _process(delta):
 		mesh_instance.scale = Vector3(stroke_width, stroke_width, stroke_width)
 #		mesh_instance.get_surface_material(0).albedo_color = stroke_color
 		get_parent().get_parent().add_child(mesh_instance)
+		
+		if signal_painted:
+			emit_signal("painted")
 	
 	timer += delta
 
@@ -39,3 +42,4 @@ func turn_on():
 	
 func turn_off():
 	is_draw = false
+
--- scripts/RobotGenerator.gd
+++ scripts/RobotGenerator.gd
@@ -8,20 +8,23 @@ export var ROBOTS_PER_ROW: int = 10
 
 var done_robots = 0
 var collisions = 0
+var paints = 0
 
 func _ready():
 	$CanvasLayer/Done.text = 'Done: ' + str(done_robots) + ' / ' + str(AMOUNT)
 	$CanvasLayer/Collisions.text = 'Collisions: ' + str(collisions / 2)
-
+	$CanvasLayer/Paint.text = 'Paints: ' + str(paints)
+	
 	for i in range(AMOUNT / ROBOTS_PER_ROW):
 		for j in range(ROBOTS_PER_ROW):
-			if i * ROBOTS_PER_ROW + j + 1 > AMOUNT:
+			if i * ROBOTS_PER_ROW + j > AMOUNT:
 				continue
 				
 			var robot = Robot.instance()
 			var brush = Brush.instance()
 			
 			brush.turn_off()
+			brush.connect("painted", self, "_on_paint_painted")
 			
 			robot.add_child(brush)
 			robot.robots_amount = AMOUNT
@@ -33,11 +36,15 @@ func _ready():
 			$ImageProcessor.connect("pass_data", robot, "_on_ImageProcessor_pass_data")
 	
 			add_child(robot)
-		
+
 func _on_robot_done():
 	done_robots += 1
-	$CanvasLayer/Done.text = 'Done: ' + str(done_robots)
+	$CanvasLayer/Done.text = 'Done: ' + str(done_robots) + ' / ' + str(AMOUNT)
 
 func _on_robot_collide():
 	collisions += 1
 	$CanvasLayer/Collisions.text = 'Collisions: ' + str(collisions / 2)
+
+func _on_paint_painted():
+	paints += 1
+	$CanvasLayer/Paint.text = 'Paints: ' + str(paints)
